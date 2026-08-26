2ecc13ef53e1f81b891235d28219a175c7a68961
AidanHerrera1 <aoherrera14@gmail.com>
2026-08-26 10:09:18 -0500

    add first 3 assignments

---
+++ madlibs.py
@@ -0,0 +1,44 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Copy and paste THIS comment from opening to closing quotes).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+
+# ℹ︎ Declare variables
+
+
+
+
+
+
+# ℹ️ Get user input and assign to variables
+object= input("please enter a type of object : ")
+location = input("please enter a location: ")
+valuable_item = input("please enter a valuable item: ")
+
+
+
+
+
+# ℹ️ Output
+# twinkle twinkle little star
+print(f"\n\ntwinkle twinkle little {object},")
+print("how I wonder what you are")
+print(f"up above the {location} so high ")
+print(f"like a {valuable_item} in the sky") 
+
+
+
+
+
+
+
+
+
